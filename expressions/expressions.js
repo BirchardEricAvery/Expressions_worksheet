1310daebe6fcd1195da11d3999c2639271d8f83b
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-09 22:30:38 -0700

    Expressions do over

---
+++ expressions/expressions.js
@@ -0,0 +1,36 @@
+/**
+ * Created by avery on 3/9/15.
+ */
+/**
+ * Created by Eric Avery Birchard on 3/9/15.
+ * Project Expressions
+ /**
+ *
+ */
+//Calculate how old Sparky the pit bull is in dog years.
+
+//Sparky's age
+
+var ageDog;
+
+var ageDog = prompt("What is Sparky's' age?");
+
+var age;
+
+var age = ageDog;
+ageDog = ageDog * 7;
+
+
+alert("Sparky's' age in human years is" + age + "which is" + ageDog +
+"in dog years");
+console.log(ageDog);
+console.log(age);
+
+
+
+//Sparky's age in dog years
+
+//Sparky is X human years old which is X in dog years
+
+
+//the average is the sum of the items divided by the number of items
