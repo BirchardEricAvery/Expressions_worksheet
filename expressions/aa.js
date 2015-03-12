eae6035aec63c9ffda5ba2f4b1b3de412819534d
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-11 17:35:25 -0700

    Moved files into correct folder and stump coded
    Stump coding complete enough

--- expressions/aa.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by avery on 3/11/15.
- */
--- expressions/js/java.js
+++ expressions/js/java.js
@@ -5,4 +5,32 @@
  Class:      SDI 1503
  Term:       1
  */
-alert("test");
+
+//alert("test"); First js file test
+
+//Fuel mileage calculator and gas left in tank
+
+//variables
+
+    //TODO: declare variables
+
+var startingGallons      //number variable
+var vehicleType          //sting variable
+var isThereGasLeftOver   //boolean variable
+
+
+//TODO: prompts
+//data types number, string, boolean
+startingGallons = prompt("Starting fuel in tank"); //number input from user
+
+vehicleType = prompt("What type of vehicle do you drive?"); //string input from user
+isThereGasLeftOver = true; //boolean input from user
+
+//casting
+//parenthesis for order of operations
+//outputs
+
+//Concatenations
+
+//TODO: console logs
+
